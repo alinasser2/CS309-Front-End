b20c728633bb06c71c8ca6cc7d02e501ded65fd1
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-12 02:53:53 +0200

    adding App to root

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
 import App from './App';
 import {BrowserRouter,Route} from 'react-router-dom';
-import Axios from "./Services/axiosService";
-import AxiosContext from "./Services/context";
+// import Axios from "./Services/axiosService";
+// import AxiosContext from "./Services/context";
 
 
 
-function index() {
-  return (
-    <div>index</div>
-  )
-}
-
-export default index
+ReactDom.render(<App/>,document.getElementById('root'))

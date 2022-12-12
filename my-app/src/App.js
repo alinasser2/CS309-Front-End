04efc7ef3d7c52275addd2cf52e3f29fd4dee1b7
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-12 02:52:40 +0200

    adding pages and url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import App from './App';
-import {BrowserRouter,Route} from 'react-router-dom';
-import Axios from "./Services/axiosService";
-import AxiosContext from "./Services/context";
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Home from './Pages/Home';
+import login from './Pages/login';
+import regester from './Pages/regester';
+import ReactDom from "react-dom";
+// import {} from '';
+// import Axios from "./Services/axiosService";
+// import AxiosContext from "./Services/context";
+
+
+ function App() {
+  return (
+    <BrowserRouter>
+    <Switch>
+      <Route path='/login' component={login}/>
+      <Route path='/regester' component={regester}/>
+      {/* <Route path='/' component={}/> */} 
+       <Route exact path='/' component={Home}/>
+    </Switch>
+    </BrowserRouter>
+  );
+ }
+
+export default App

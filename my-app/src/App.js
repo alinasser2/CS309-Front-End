b051732524ce1dafdc5ab1318dbae7098723e004
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-14 01:27:52 +0200

    using modern tools

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import React from "react"
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Regester from './Pages/Regester'
+import Login from './Pages/login'
 import Home from './Pages/Home';
-import login from './Pages/login';
-import regester from './Pages/regester';
-import ReactDom from "react-dom";
-// import {} from '';
-// import Axios from "./Services/axiosService";
-// import AxiosContext from "./Services/context";
+// // import {} from '';
+// // import Axios from "./Services/axiosService";
+// // import AxiosContext from "./Services/context";
+
 
 
  function App() {
-  return (
+  
+ 
+    return (
     <BrowserRouter>
-    <Switch>
-      <Route path='/login' component={login}/>
-      <Route path='/regester' component={regester}/>
-      {/* <Route path='/' component={}/> */} 
-       <Route exact path='/' component={Home}/>
-    </Switch>
-    </BrowserRouter>
-  );
- }
 
-export default App
+   <Routes>
+   <Route excet path="/" element={<Home/>} />
+   <Route path="/login" element={<Login/>} />
+   <Route  path="/regester" element={<Regester/>} /> 
+    {/* <Route path='/' element={}/> */} 
+   </Routes>
+   </BrowserRouter> 
+
+
+      );
+    }
+    export default App;

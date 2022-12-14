c3bc0b4596575ac9e75fb1847fa890858f553257
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-14 02:37:07 +0200

    renaming

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,9 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Regester from './Pages/Regester'
 import Login from './Pages/login'
 import Home from './Pages/Home';
-// // import {} from '';
-// // import Axios from "./Services/axiosService";
-// // import AxiosContext from "./Services/context";
+ // import {} from '';
+ // import Axios from "./Services/axiosService";
+ // import AxiosContext from "./Services/context";
 
 
 
@@ -15,12 +15,12 @@ import Home from './Pages/Home';
     return (
     <BrowserRouter>
 
-   <Routes>
-   <Route excet path="/" element={<Home/>} />
-   <Route path="/login" element={<Login/>} />
-   <Route  path="/regester" element={<Regester/>} /> 
-    {/* <Route path='/' element={}/> */} 
-   </Routes>
+        <Routes>
+            <Route excet path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route  path="/Regester" element={<Regester/>} /> 
+            {/* <Route path='/' element={}/> */} 
+        </Routes>
    </BrowserRouter> 
 
 

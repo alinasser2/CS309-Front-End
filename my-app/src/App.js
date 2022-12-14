7c9c9867b872b41e4614a14fd40c7291313bf55d
aliyasser <oy36578@gmail.com>
2022-12-14 15:22:07 +0200

    App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,30 @@
-import React from "react"
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
-import Regester from './Pages/Regester'
-import Login from './Pages/Login'
-import Home from './Pages/Home';
- // import {} from '';
- // import Axios from "./Services/axiosService";
- // import AxiosContext from "./Services/context";
-
-
-
- function App() {
+//*//
+import React from "react";
+import {BrowserRouter,Route,Routes,Link} from 'react-router-dom';
+import Register from './Register'
+import Login  from './Login'
+import Home from './Home'
+function App() {
   
  
     return (
-  
- 
+ <BrowserRouter>
+<Link to="/">Home</Link> 
+<hr />
+<Link to="/Login">Login</Link> 
+<hr />
+<Link to="/Register">Register</Link> 
+<hr />
 
-    <BrowserRouter>
+   
+   <Routes>
+   
+   <Route  excet path="/" element={<Home/>} />
 
-        <Routes>
-            <Route excet path="/" element={<Home/>} />
-            <Route path="/Login" element={<Login/>} />
-            <Route  path="/Regester" element={<Regester/>} /> 
-            {/* <Route path='/' element={}/> */} 
-        </Routes>
-   </BrowserRouter> 
-
-
-      );
+    <Route  path="/Login" element={<Login/>} />
+    <Route path="/Register" element={<Register/>} />  
+         </Routes>
+  </BrowserRouter> 
+  );
     }
     export default App;

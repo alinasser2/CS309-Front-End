6b5f2e28b4b9ef07caa26d222d2ca7d19e214a23
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-14 02:36:21 +0200

    making schema for checking the info in log and reg

--- my-app/src/Pages/login.js
+++ my-app/src/Pages/login.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React,{UseState} from 'react';
+import  Input  from '../components/Input';
+import valit from 'valit';
 
 function login() {
+  const[data,setData]= UseState({
+    email:'',
+    password:''
+  });
+
+  const[errors,setErrors]= UseState({})
+
+  const schema ={ 
+    username: valit.creatSchema(valit.string,valit.isRequiered,{
+        min:5,
+        max:30,
+        label:'Username',
+
+    }),
+     password:valit.creatSchema(valit.string,valit.isRequiered,{
+      min:8,
+      max:50,
+      label:'Password',
+
+  })
+
+
+
+  }
   return (
+    <>
     <h1>login</h1>
+    <Input label="Username" inputName="username" data={data}/>
+    </>
   );
 }
 
--- my-app/src/Pages/regester.js
+++ my-app/src/Pages/regester.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React,{UseState} from 'react'
+import  Input  from '../components/Input';
+import valit from 'valit';
 
-function regester() {
+function Regester() {
+
+  const[data,setData]= UseState({
+    username:'',
+    email:'',
+    password:'',
+    confirm_password:'',
+    phone:'',
+    city:'',
+    state:''
+
+  });
+
+  const[errors,setErrors]= UseState({})
+
+  const schema ={ 
+    username: valit.creatSchema(valit.string,valit.isRequiered,{
+        min:5,
+        max:30,
+        label:'Username',
+
+    }),
+     password:valit.creatSchema(valit.string,valit.isRequiered,{
+      min:8,
+      max:50,
+      label:'Password',
+
+  })
+
+
+
+  }
+
   return (
-    <h1>regester</h1>
+      <>
+    <h1>cxmf</h1>
+    <Input label="Username" inputName="username" data={data}/>
+     </>
   );
 }
 
-export default regester
+export default Regester

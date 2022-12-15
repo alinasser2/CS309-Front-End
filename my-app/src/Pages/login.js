1a597ed045b9b9ad067f35f00bbc7c3d6449cc19
aliyasser <oy36578@gmail.com>
2022-12-15 14:59:25 +0200

    Login

--- my-app/src/Pages/login.js
+++ my-app/src/Pages/login.js
@@ -1,49 +1,9 @@
-// import React,{UseState} from 'react';
-// import  Input  from '../components/Input';
-// import valit from 'valit';
-
-// function Login() {
- 
-
-//   const[errors,setErrors]= UseState({})
-
-//   const schema ={ 
-//     username: valit.creatSchema(valit.string,valit.isRequiered,{
-//         min:5,
-//         max:30,
-//         label:'Username',
-
-//     }),
-//      password:valit.creatSchema(valit.string,valit.isRequiered,{
-//       min:8,
-//       max:50,
-//       label:'Password',
-
-//   })
-
-
-
-//   }
-//   return (
-//     <>
-//     <h1>login</h1>
-//     <Input label="Username" inputName="username" data={data}/>
-//     </>
-//   );
-// }
-
-// export default Login
-
 import React, { useState } from "react";
 import {Button} from '@mui/material';
-import  Input  from '../components/Input';
- const Login = (props) => {
+
+const Login = (props) => {
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const[data,setData]= useState({
-        email:'',
-        password:''
-      });
+    const [pass, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -52,22 +12,19 @@ import  Input  from '../components/Input';
     }
 
     return (
-        <div className="form-container">
+        <div className="component">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label className="email">
                    <h3> Email</h3>
                     </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com"  name="email" required minLength={13} maxLength={25}/>
                 <label className="password">
                 <h3> password</h3>
                 </label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <Input label="Username" inputName="username" data={data} handleChange={handleSubmit} placeholder="Username"/>
-                <Input label="Password" inputName="Password" data={data} handleChange={handleSubmit} placeholder="Password"/>
+                <input value={pass} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" name="password"   required minLength={5} maxLength={10}/>
                 <Button variant="contained" type="submit">Log In</Button>
             </form>
-            
         </div>
     )
 }

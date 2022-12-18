c76701713ded7e5631e5e2a5c15a74900302d959
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-18 20:33:45 +0200

    puting products in home page

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,8 +1,59 @@
-import React from 'react';
-
+import React from "react";
+import Post from "../components/Post";
+import {
+  Typography,
+  Grid,
+  CssBaseline,
+  Container,
+  link,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 function Home() {
   return (
-    <h1>Home</h1>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={4} >
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+        <Grid item xs={4}>
+          <Post painte_name="mounir" price="10000" />
+        </Grid>
+       
+      </Grid>
+    </Container>
   );
 }
 

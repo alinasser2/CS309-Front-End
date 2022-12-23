4081da46e321d1d80ad256138b3cba577f71ec5e
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-23 04:33:59 +0200

    fixing  products in home page & handling product

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState, useEffect }from "react";
 import Post from "../components/Post";
 import {
   Typography,
@@ -10,48 +10,38 @@ import {
   CardActionArea,
   CardMedia,
   CardContent,
+  Button,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 function Home() {
+
+  const Url = "";
+  const [num,setNum]=useState([]);
+
+  const use=[{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7},{id:8},{id:9},{id:10},{id:11}]
+  useEffect(()=>{
+    setNum(use);
+  return()=>{
+  };
+  },[]);
+
+  let handleAddProduct = () => {
+
+  };
   return (
     <Container>
+      <CssBaseline/>
       <Grid container spacing={2}>
-        <Grid item xs={4} >
-          <Post painte_name="mounir" price="10000" />
+      {num.map(idP=>(
+        <Grid item sm={6} xs={12} md={4}>
+        <Post painte_name="mounir" price="10000" handleClick={`/Product/${idP.id}`} handleAdd={handleAddProduct} />
         </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
+    ))}
+        <Grid item xs={12}>
+          <Button variant="contained" href="/ProductList">
+           View More Products
+          </Button>
         </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-        <Grid item xs={4}>
-          <Post painte_name="mounir" price="10000" />
-        </Grid>
-       
       </Grid>
     </Container>
   );

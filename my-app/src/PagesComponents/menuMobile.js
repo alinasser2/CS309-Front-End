21f2c08aea9951d2a915b78164af74313b91d992
aliyasser <oy36578@gmail.com>
2022-12-23 14:58:21 +0200

    menuMobile

---
+++ my-app/src/PagesComponents/menuMobile.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+const pages=[{title:'Products',id:"./Products"},{title:'REGISTER',id:"./REGISTER"},{title:'LOGIN',id:"./LOGIN"},{title:'About',id:"./About"}];
+const mobile = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  return (
+    <React.Fragment>
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        <List>
+          {pages.map((page, index) => (
+            <ListItemButton key={index}>
+              <ListItemIcon>
+              <a href={page.id} style={{color:"black"}} >
+              <ListItemText style={{color:"black"}}>{page.title}</ListItemText>
+                </a>
+                
+                
+           
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ color: "white", marginLeft: "auto" }}
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon color="white" />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+
+export default mobile;

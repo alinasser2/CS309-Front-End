d1248bfcc6fca685d2ac5100cfbbc3184000f2dc
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-23 04:35:38 +0200

    handling onclick and links

--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Typography, Grid, CssBaseline, Container, link, Card, CardActionArea, CardMedia, CardContent } from "@mui/material";
+import {Link} from 'react-router-dom';
+import { Typography, Grid, CssBaseline, Container, link, Card, CardActionArea, CardMedia, CardContent,Button,CardActions } from "@mui/material";
 import { makeStyles} from '@mui/styles';
-
-function Post({imgURL ,painte_name , price,imgDescription='product'}) {
+function Post({imgURL ,painte_name , price,handleClick,handleAdd,...rest}) {
   return (
     <>
 
     <Card sx={{ maxWidth: 345 }}>
-    <CardActionArea>
+     
+    <CardActionArea > 
+     <Link to={handleClick}>
       <CardMedia
         component="img"
         height="20%"
         image="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt={imgDescription}
-      />
+        alt={painte_name}
+    
+        />
+      </Link>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
          {painte_name}
@@ -21,8 +25,11 @@ function Post({imgURL ,painte_name , price,imgDescription='product'}) {
         <Typography variant="body2" color="text.secondary">
          price  {price}
         </Typography>
-      </CardContent>
+   </CardContent>
     </CardActionArea>
+      <CardActions>      
+      <Button size="small"   onClick={handleAdd}>Add product to Card</Button>   
+    </CardActions>
   </Card>
   </>
   );

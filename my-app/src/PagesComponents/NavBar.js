d072d8e05b24cf42c0a1e76e25dbcf72d85e1621
aliyasser <oy36578@gmail.com>
2022-12-24 15:10:52 +0200

    upDate-NavBar

--- my-app/src/PagesComponents/NavBar.js
+++ my-app/src/PagesComponents/NavBar.js
@@ -10,7 +10,7 @@ import {
   useTheme,alpha,Menu,MenuItem
 } from "@mui/material";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCartSharp';
-import mobile from "./menuMobile";
+import Mobile from "./menuMobile";
 import logo from './Assts/logo.png'
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
@@ -18,10 +18,9 @@ import Tooltip from '@mui/material/Tooltip';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import ReorderSharpIcon from '@mui/icons-material/ReorderSharp';
 import MenuIcon from '@mui/icons-material/Menu';
-import './Navar.css'
+import './NavBar.css'
 import { Link } from 'react-router-dom';
-import Test from "./Test";
-const settings=[{title:'Products',id:"./Products"},{title:'REGISTER',id:"./REGISTER"},{title:'LOGIN',id:"./LOGIN"},{title:'About',id:"./About"}];
+const settings=[{title:'Products',id:"./"},{title:'REGISTER',id:"./REGISTER"},{title:'LOGIN',id:"./LOGIN"},{title:'About',id:"./About"}];
 // ['Products','REGISTER','LOGIN','About'];
 // const hj=['./'];
 const Logo=styled('img')(()=>({
@@ -35,12 +34,12 @@ const Logo=styled('img')(()=>({
     '&:hover': {
       backgroundColor: alpha(theme.palette.common.white, 0.35),
     },
-    marginRight: theme.spacing(0),
+    marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
     minWidth:'300px',
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(-175),
+      marginLeft: theme.spacing(-230),
       width: 'auto',
     },
   }));
@@ -93,7 +92,7 @@ const Logo=styled('img')(()=>({
 
   return (
     <React.Fragment>
-      <AppBar sx={{ background: '#00b4d8'}}>
+      <AppBar sx={{ background: '#00b4d8', height:'79px'}}>
         <Toolbar>
         <Logo src={logo} />
        
@@ -102,18 +101,18 @@ const Logo=styled('img')(()=>({
               <Typography sx={{ fontSize: "2rem", paddingLeft: "10%" }}>
                 Shopping
               </Typography>
-              <DrawerComp />
+              <Mobile />
             </>
           ) : (
             <>
               <Tabs
-                sx={{ marginLeft: "63%" }}
+                sx={{ marginLeft: "70%" }}
                 indicatorColor="secondary"
                 textColor="inherit"
                 value={value}
                 onChange={(e, value) => setValue(value)}
               >
-               <a href="./Procects" >
+               <a href="./" >
                  < Tab className="links" label="Procects"  />
                  </a>
                  <a href="./REGISTER" >
@@ -128,7 +127,7 @@ const Logo=styled('img')(()=>({
                 
               </Tabs>
             
-         <Box sx={ {marginLeft: "1" } }>
+         {/* <Box sx={ {marginLeft: "1" } }>
             <IconButton style={{color:'white',marginLeft:'10px'}}>
             <a href="https://www.google.com/" >
             <ShoppingCartIcon/>
@@ -136,7 +135,35 @@ const Logo=styled('img')(()=>({
             
             </IconButton>
           
-          </Box>
+          </Box> */}
+          <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+            onClick={"https://www.google.com/"}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              fill="currentColor"
+            >
+              <path d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z" />
+            </svg>
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-item-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              {5}
+            </div>
+          </Button>
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 1}} style={{color:'white'}}>

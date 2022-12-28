1eecd00e7577f60c4fc0a40b26ca51220be6c73b
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2022-12-28 23:36:34 +0200

    connect log in to back

--- my-app/src/Pages/login.js
+++ my-app/src/Pages/login.js
@@ -1,31 +1,50 @@
 import React, { useState } from "react";
-import {Button} from '@mui/material';
-
-const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPassword] = useState('');
-
+import axios from "axios";
+const Login = () => {
+    const [data, setData] = useState({
+      email: "",
+      password: ""
+    });
+  
+    const handleChange = (e) => {
+      const value = e.target.value;
+      setData({
+        ...data,
+        [e.target.name]: value
+      });
+    };
+  
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("the Email is:"+email);
-        console.log("the password is:"+pass);
-    }
+      e.preventDefault();
+      const userData = {
+        email: data.email,
+        password: data.password
+      };
+      axios
+        .post("http://localhost:3001/api/auth/login", userData)
+        .then((response) => {
+          console.log(response.status);
+          console.log(response.data.message);
+        })
 
+    };
+  
     return (
-        <div className="component">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label className="email">
-                   <h3> Email</h3>
-                    </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com"  name="email" required minLength={13} maxLength={25}/>
-                <label className="password">
-                <h3> password</h3>
-                </label>
-                <input value={pass} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" name="password"   required minLength={5} maxLength={10}/>
-                <button  type="submit">Log In</button>
-            </form>
-        </div>
-    )
-}
-export default Login
+        <div classemail="component">
+                    <h2>Login</h2>
+                       <form classemail="login-form" onSubmit={handleSubmit}>
+                        <label classemail="email">
+                             <h3> Email</h3>
+                               </label>
+                          <input value={data.email}  name="email"onChange={handleChange}type="email" placeholder="youremail@gmail.com"  email="email" required minLength={13} maxLength={25}/>
+                         <label classemail="password">
+                           <h3> password</h3>
+                         </label>
+                          <input  value={data.password}    name="password" onChange={handleChange} type="password" placeholder="password" email="password"   required minLength={5} maxLength={10}/>
+                          <button  type="submit">Log In</button>
+                     </form>
+                  </div>
+
+    );
+  };
+  export default Login
